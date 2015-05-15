d20ecacc51b94ab055a6f5e1552c011daf458013
Perttu Ristimella <perttu.ristimella@gmail.com>
2015-05-15 19:28:33 +0300

    linkkivaihdos ja outbound links

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 $(function() {
-$('html').removeClass('no-js');
+  'use strict';
+  $('html').removeClass('no-js');
 
     //code here
 
@@ -26,9 +27,41 @@ $('html').removeClass('no-js');
     $("#navigation").addClass('nav-closed');
     $("html, body").animate({scrollTop:top}, '700', function() {
     });
-  }
+  };
   $("#navigation").on('click', 'a', scrollDown);
 
   $("body").on('click', '.smooth-scroll', scrollDown);
 
+  var baseURI = window.location.host;
+  
+
+  $("body").on("click", function(e) {
+  
+    // abandon if link already aborted or analytics is not available
+    if (e.isDefaultPrevented() || typeof ga !== "function") return;
+  
+    // abandon if no active link or link within domain
+    var link = $(e.target).closest("a");
+    if (link.length !== 1 || baseURI === link[0].host) return;
+  
+
+    e.preventDefault();
+    var href = link[0].href;
+    ga('send', {
+      'hitType': 'event',
+      'eventCategory': 'outbound',
+      'eventAction': 'link',
+      'eventLabel': href,
+      'hitCallback': loadPage
+    });
+
+    // redirect after one second if recording takes too long
+    setTimeout(loadPage, 1000);
+
+    // redirect to outbound page
+    function loadPage() {
+      document.location = href;
+    }
+  
+  });
 });
--- texts/koodiviidakko.md
+++ texts/koodiviidakko.md
@@ -1,4 +1,4 @@
-### Koodiviidakko
+### Lianatech (Koodiviidakko)
 
 **When:** 2012 - 2013
 
@@ -8,7 +8,7 @@
 
 I worked as a lead front-end developer and lead designer in multiple project ranging from e-commerce and websites, to Javascript UIs and HTML newsletters. I started out as an intern and improved my developer skills really fast. In no time, I was able to handle complex website projects and guide new interns as well. In addition to basic front-end projects, I was trusted to develop more special and tailored solutions, such as Javascript UI’s and designing UI for Viidakko's Intranet. I was also a key developer of a new Javascript product filtering plugin, which increased the speed of browsing products and decreased the server workload significantly.
 
-[www.viidakko.fi](http://www.viidakko.fi "Visit Koodiviidakko's site")
+[www.lianatech.com](http://www.lianatech.com "Visit Lianatech's site") **&** [www.lianamailer.com](http://www.lianamailer.com/ "Visit Lianamailer site")
 
 
 
